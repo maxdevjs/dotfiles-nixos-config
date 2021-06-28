2a8cd661a77f6251e95d9a49c6ad1133d837aa0b
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2021-06-28 04:18:37 -0300

    Switch to nix-community/neovim-nightly-overlay

--- dev/editors.nix
+++ dev/editors.nix
@@ -7,9 +7,11 @@
         brackets
         kakoune
         kate
-        neovim
-        neovim-qt
-        vimHugeX 
+        
+        # https://github.com/nix-community/neovim-nightly-overlay
+        neovim-nightly # activated in dev/editors.nix
+        #neovim
+        #vimHugeX 
         #vscode-with-extensions: editors.vscode_UNSTABLE.nix
         #vscodium: editors.vscodium_UNSTABLE.nix
       ];
--- overlays/default.nix
+++ overlays/default.nix
@@ -3,9 +3,12 @@
 {
   nixpkgs.overlays = [
     #(import ./st/st.nix)
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
   ];
 
   environment.systemPackages = with pkgs; [
-    st
+    #st
   ];
 }
