fc96ff6500bab871eb0c35c05f69a93dbe2c5469
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2021-08-21 19:41:39 -0300

    Clean up

--- dev/editors.nix
+++ dev/editors.nix
@@ -10,9 +10,7 @@
         
         # https://github.com/nix-community/neovim-nightly-overlay
         #neovim-nightly # it compiles from sources, time to switch to unstable
-        #vimHugeX 
-        #vscode-with-extensions: editors.vscode_UNSTABLE.nix
-        #vscodium: editors.vscodium_UNSTABLE.nix
+        #vimHugeX
       ];
   };
 }
