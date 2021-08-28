6977daab88c216164d8770f01953406dc9000de6
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2021-08-27 21:29:45 -0300

    Add unstable.google-chrome-dev

--- unstable/unstable.nix
+++ unstable/unstable.nix
@@ -17,7 +17,7 @@ in {
         unstable.google-chrome
         #patchelf: getting info about '/nix/store/03qqrijxqbgyp01zxr8n8klm2ch0icqk-google-chrome-dev-94.0.4603.0/share/google/chrome-unstable/crashpad_handler': No such file or directory
         #builder for '/nix/store/g7drfpyms4pnkqa2nrq2j711lfpgh5r1-google-chrome-dev-94.0.4603.0.drv' failed with exit code 1
-        #unstable.google-chrome-dev
+        unstable.google-chrome-dev
         unstable.google-chrome-beta
         unstable.inkscape
         unstable.jack2
