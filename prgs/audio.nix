397a837d9216db56deebf589d18b669cf529957c
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2021-10-18 17:28:06 -0300

    Set prgs/audio/unstable.sunvox

--- prgs/audio.nix
+++ prgs/audio.nix
@@ -1,6 +1,28 @@
-{ config, pkgs, ...}: {
+# { config, pkgs, ...}: {
+#   environment = {
+#     systemPackages = with pkgs;
+#       [
+        #audacity
+        #klystrack
+        #lmms
+        #milkytracker
+        #mopidy
+        #soundtracker
+#         sunvox
+#       ];
+#   };
+# }
+
+
+{ config, pkgs, ... }:
+
+# https://discourse.nixos.org/t/installing-only-a-single-package-from-unstable/5598/2
+
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in {
   environment = {
-    systemPackages = with pkgs;
+    systemPackages = with pkgs; 
       [
         #audacity
         #klystrack
@@ -8,7 +30,7 @@
         #milkytracker
         #mopidy
         #soundtracker
-        sunvox
+        unstable.sunvox
       ];
   };
 }
